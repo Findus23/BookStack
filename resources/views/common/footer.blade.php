f7558d48a25a8ac40626cc56dc4a6df09778c62a
Lukas Winkler <git@lw1.at>
2020-04-11 14:01:14 +0200

    more footer links

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -1,4 +1,4 @@
-@if(setting('app-privacy-policy') | setting('app-terms-of-service'))
+@if(setting('app-privacy-policy') or setting('app-terms-of-service'))
 <footer>
     @if(setting('app-privacy-policy'))
         <a href="{{ setting('app-privacy-policy') }}">{{ trans('settings.app_privacy_policy') }}</a>
@@ -6,5 +6,7 @@
     @if(setting('app-terms-of-service'))
         <a href="{{ setting('app-terms-of-service') }}">{{ trans('settings.app_terms_of_service') }}</a>
     @endif
+    <a href="https://www.bookstackapp.com/">Powered by BookStack</a>
+    <a href="https://github.com/Findus23/BookStack/">with custom modifications</a>
 </footer>
 @endif
